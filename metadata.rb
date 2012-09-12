6a5702bb3fa39c82bcfa1af7ddc419c02d1cd2f5
Graham McIntire <gmcintire@gmail.com>
2012-09-12 10:40:37 -0400

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Terenine"
+maintainer_email "YOUR_EMAIL"
+license          "Apache 2.0"
+description      "Installs/Configures apollo"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
